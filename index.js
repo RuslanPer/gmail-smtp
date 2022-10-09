28c06fd720c209c9550def168a7687edc6c55fcc
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-10-09 21:28:31 +0300

    feat: first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const port = 3010
+
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'pershin.ruslan98@gmail.com', // generated ethereal user
+		pass: 'rzutodosvexyvaoz', // generated ethereal password
+	},
+})
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.post('/sendMessage', async (req, res) => {
+	let { name, email, message } = req.body
+
+	let info = await transporter.sendMail({
+		from: 'HR', // sender address
+		to: 'ruslan.pershin98@gmail.com', // list of receivers
+		subject: 'Тестирую gmail', // Subject line
+		html: `<b>Сообщение из портфолио: </b> <div>${message}</div> <div>name: ${name}</div> <div>email: ${email}</div>	`, // html body
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
